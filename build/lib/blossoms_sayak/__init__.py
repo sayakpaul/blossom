c680cbccc4020b5b512f6f0429b86dd519c47865
sayakpaul <spsayakpaul@gmail.com>
2024-03-10 11:51:49 +0530

    major updates

--- build/lib/blossoms_sayak/__init__.py
+++
@@ -1,4 +0,0 @@
-__version__ = "0.8.0"
-
-from .geometry import circle_area
-from .operations import add, subtract, multiply, divide, mm 
--- build/lib/blossoms_sayak/geometry.py
+++
@@ -1,8 +0,0 @@
-import math
-
-def circle_area(radius):
-    return math.pi * radius ** 2
-
-
-def circle_circumference(radius):
-    return 2 * math.pi * radius
