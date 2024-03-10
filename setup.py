a4fb7a980841018e3e9574bcfd2d11d5291758d9
sayakpaul <spsayakpaul@gmail.com>
2024-03-10 10:42:13 +0530

    changes

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="blossoms-sayak",
+    version="0.1.0",
+    description="A simple Python package for math operations and geometry calculations.",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
